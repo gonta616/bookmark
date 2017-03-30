8862427b21a69ec33dc892245a7aae55bf5c0b45
gonta616 <k-gonjo@cec-ltd.co.jp>
2017-03-30 19:39:06 +0900

    Success assetic src

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new Sp\BowerBundle\SpBowerBundle(),
             new AppBundle\AppBundle(),
         ];
 
